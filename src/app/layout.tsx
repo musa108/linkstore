abe21818bb2e205d17b7f9ce8dd63e14078fb475
musa108 <nmanagi23@gmail.com>
2026-04-01 14:30:42 +0100

    feat: add Vercel Analytics and implement root layout with Clerk and font configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 
 const geistSans = Geist({
@@ -33,6 +34,7 @@ export default function RootLayout({
           {children}
         </body>
         <SpeedInsights/>
+        <Analytics/>
       </html>
     </ClerkProvider>
   );
